c89d1529d4d90b3c0b5d51c69aa9addcc1564dcd
Ricardo Ludwig <rikevere@gmail.com>
2024-02-09 17:27:29 -0300

    gerenciando histórico com assitente e thread

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 from time import sleep
 from helpers import *
 from selecionar_persona import *
-from selecionar_documentos import *
+from assistente_ecomart import *
 
 load_dotenv()
 
@@ -15,50 +15,35 @@ modelo = "gpt-4"
 app = Flask(__name__)
 app.secret_key = 'alura'
 
-#contexto = carrega("chatbotPython\dados\ecomart.txt") 
+assistente = criar_assistente()
+thread = criar_thread()
 
 def bot(prompt):
     maximo_tentativas = 1
     repeticao = 0
-    #personas, refere-se a uma coleção/dicionário presente em "selecionar_persona"
-    #permite que através de um índice, seja retornado um valor de texto ou numérico.
-    #no exemplo a seguir, através do dado da variável "prompt", é retornado o índice para a coleção e seu respectivo valor
-    personalidade = personas[selecionar_persona(prompt)]
-    contexto = selecionar_contexto(prompt)
-    documento_selecionado = selecionar_documento(contexto)
-
     while True:
         try:
-            prompt_do_sistema = f"""
-            Você é um chatbot de atendimento a clientes de um e-commerce. 
-            Você não deve responder perguntas que não sejam dados do ecommerce informado!
-            Você deve gerar respostas utilizando o contexto abaixo.
-            Você deve adotar a persona abaixo.
-            
-            # Contexto
-            {documento_selecionado}
+            cliente.beta.threads.messages.create(
+            thread_id=thread.id,
+            role = "user",
+            content =  prompt
+            )
 
-            # Persona
-            {personalidade} 
-            """
-            response = cliente.chat.completions.create(
-                messages=[
-                        {
-                                "role": "system",
-                                "content": prompt_do_sistema
-                        },
-                        {
-                                "role": "user",
-                                "content": prompt
-                        }
-                ],
-                temperature=1,
-                max_tokens=300,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0,
-                model = modelo)
-            return response
+            run = cliente.beta.threads.runs.create(
+                    thread_id=thread.id,
+                    assistant_id=assistente.id
+                )
+            
+            while run.status !="completed":
+                run = cliente.beta.threads.runs.retrieve(
+                    thread_id=thread.id,
+                    run_id=run.id
+                )
+
+            historico = list(cliente.beta.threads.messages.list(thread_id=thread.id).data)
+            resposta = historico[0]
+            return resposta    
+            
         except Exception as erro:
                 repeticao += 1
                 if repeticao >= maximo_tentativas:
@@ -70,12 +55,21 @@ def bot(prompt):
 def chat():
     prompt = request.json["msg"]
     resposta = bot(prompt)
-    texto_resposta = resposta.choices[0].message.content
+    texto_resposta = resposta.content[0].text.value
     return texto_resposta
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+#Detectar quando um usuário fecha uma página do navegador
+@app.route('/logoff', methods=['POST'])
+def logoff():
+    # Aqui você pode adicionar a lógica para tratar o logoff, como atualizar o status do usuário no banco de dados
+    cliente.beta.assistants.delete(assistant_id=assistente.id)
+    cliente.beta.threads.delete(thread_id=thread.id)
+    print("Usuário saiu")
+    return '', 204  # Retorna uma resposta sem conteúdo
 
 if __name__ == "__main__":
     app.run(debug = True)
--- assistente_ecomart.py
+++ assistente_ecomart.py
@@ -9,60 +9,26 @@ load_dotenv()
 
 cliente = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 modelo = "gpt-4"
-contexto = carrega("chatbotPython/dados/ecomart.txt")
-#1. - Primeiro, criamos um assistente chamado "Atendente EcoMart", que segue um contexto e tem uma persona definida
-assistente = cliente.beta.assistants.create(
-    name="Atendente EcoMart",
-    instructions = f"""
-        Você é um chatbot de atendimento a clientes de um e-commerce. 
-        Você não deve responder perguntas que não sejam dados do ecommerce informado!
-        Além disso, adote a persona abaixo para responder ao cliente.
-        
-        ## Contexto
-        {contexto}
-        
-        ## Persona
-        
-        {personas["neutro"]}
-    """,
-    model = modelo,
-)
-#2. - Após a criação do assistente, criamos uma thread para a sessão de conversa entre o assistente e um usuário
-thread = cliente.beta.threads.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Liste os produtos"
-        }
-    ]
-)
-#2.1 - Após a criação da thread, incluímos mais informações de prompt na sessão de conversa entre o assistente e um usuário
-cliente.beta.threads.messages.create(
-    thread_id=thread.id,
-    role = "user",
-    content =  " da categoria moda sustentável"
-)
-#3. Executamos o assistente no thread criado para processar a mensagem do usuário
-run = cliente.beta.threads.runs.create(
-    thread_id=thread.id,
-    assistant_id=assistente.id
-)
-#4. - Incluímos um loop para verificar o status da execução até que seja completada.
-while run.status !="completed":
-    run = cliente.beta.threads.runs.retrieve(
-        thread_id=thread.id,
-        run_id=run.id
+contexto = carrega("dados/ecomart.txt")
+
+def criar_thread():
+    return cliente.beta.threads.create()
+
+def criar_assistente():
+    assistente = cliente.beta.assistants.create(
+        name="Atendente EcoMart",
+        instructions = f"""
+            Você é um chatbot de atendimento a clientes de um e-commerce. 
+            Você não deve responder perguntas que não sejam dados do ecommerce informado!
+            Além disso, adote a persona abaixo para responder ao cliente.
+            
+            ## Contexto
+            {contexto}
+            
+            ## Persona
+            
+            {personas["neutro"]}
+        """,
+        model = modelo,
     )
-#5. - Após a execução, recuperamos a resposta do assistente e procedemos com a exclusão do assistente e do thread
-historico = cliente.beta.threads.messages.list(thread_id=thread.id).data
-resposta = historico[0]
-print(print(f"role: {resposta.role}\nConteúdo: {resposta.content[0].text.value}"))
-
-#cliente.beta.assistants.delete(assistant_id=assistente.id)
-#cliente.beta.threads.delete(thread_id=thread.id)
-
-
-#historico = cliente.beta.threads.messages.list(thread_id=thread.id).data
-
-#for mensagem in historico:
-#   print(mensagem.content[0].text.value)
+    return assistente
